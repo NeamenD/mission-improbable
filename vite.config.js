4b226784409be93429a10c502abaf72d953f1ce6
Neamen Dobamo <68127279+NeamenD@users.noreply.github.com>
2026-05-25 17:13:21 -0600

    thrid commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/date-meme/", // IMPORTANT: Replace 'date-meme' with your GitHub repository name
+  base: "/mission-improbable/", // IMPORTANT: Replace 'date-meme' with your GitHub repository name
 });
